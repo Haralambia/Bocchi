721db1b7f41d01134a85fe9a7419b25810809ebb
bia <hara.artmis@hotmail.com>
2022-03-10 16:40:16 +0000

    Created basic commands

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const prefix = "bocchi gib ";
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", (message) => {
-  console.log(message.content);
+  if (message.author.bot) return false;
+
+  if (message.content.startsWith(prefix)) {
+    const cleanCommand = message.content.substring(prefix.length);
+    const command = cleanCommand.split(" ");
+
+    switch (command[0]) {
+      case "wahaha": return message.channel.send("wahaha");
+      default: return message.channel.send("bocchi confused");
+    }
+  }
 });
 
 client.login(process.env.APPLICATION_TOKEN);
